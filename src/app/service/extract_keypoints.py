f471dce478ff93f60949db98589133196eff1649
rbgir1113-dot <rbgir1113@gmail.com>
2026-06-16 10:09:10 +0900

    fix: cached_keypoints 경로를 상대경로로 변경

--- src/app/service/extract_keypoints.py
+++ src/app/service/extract_keypoints.py
@@ -1,7 +1,9 @@
 import numpy as np
 import os
 
-CACHE_DIR = r"C:\back_0900_lgh\project\fastapp\resource\cached_keypoints"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+CACHE_DIR = os.path.join(BASE_DIR, "resource", "cached_keypoints")
 
 def extract_keypoints_from_json(word_name: str) -> list:
     npy_path = os.path.join(CACHE_DIR, f"{word_name}.npy")
